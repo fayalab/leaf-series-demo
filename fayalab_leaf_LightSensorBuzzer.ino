9a0cebca82170b8e80adc3ce4982571d1195f82a
fayalab <info@fayalab.com>
2014-07-11 16:50:00 +0800

    Create fayalab_leaf_LightSensorBuzzer.ino

---
+++ fayalab_leaf_LightSensorBuzzer.ino
@@ -0,0 +1,39 @@
+/*
+* fayalab leaf sample code for leaf-Light sensor and leaf-Buzzer
+* 
+* Control the Buzzer active frequency by the light sensor value
+* 
+* 2014/07/11 by Welly Lee 
+* wellylee@fayalab.com
+*/
+
+// First, define the pin depends on your connection
+// define pin for fayalab leaf-Light and leaf-Buzzer
+int pin_Lightsensor = A0;   
+
+// select the pin for the LED
+int pin_Buzzer = 13;      
+// variable to store the value coming from the sensor
+int sensorValue = 0;  
+// frequency of buzzer active 
+int frequency = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // declare the pin_Buzzer as an OUTPUT:
+  pinMode(pin_Buzzer, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(pin_Lightsensor);
+  Serial.println(sensorValue);  
+  // active the buzzse 
+  digitalWrite(pin_Buzzer, HIGH);  
+  // calculate the frequency of buzzer
+  frequency = sqrt(1023-sensorValue)*70;
+  delay(frequency);  
+  // turn off the buzzer        
+  digitalWrite(pin_Buzzer, LOW);   
+  delay(frequency);                    
+}
